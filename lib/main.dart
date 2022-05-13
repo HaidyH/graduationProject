1c7b0651d84c78343924d174b162b59d5f400aea
haidyH <sim.haidy.hossam@alexu.edu.eg>
2022-05-13 15:14:28 +0200

    d7k

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +13,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return FutureBuilder(
+        future: Authentication.initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.done)  {
+            return MaterialApp(
+              title: 'Flutter Firebase Authentication Tutorial',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const MyHomePage(title: 'Flutter Demo Home Page'),
+            );
+          }
+          return CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.orange));
+        });
   }
+
+
 }
 
 class MyHomePage extends StatefulWidget {
@@ -49,20 +57,31 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final myController = TextEditingController();
 
   void _incrementCounter() {
     setState(() {
-
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -75,18 +94,82 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              autofocus: true,
+              controller: myController ,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: (){Authentication.signup(context, myController.text, "123123");},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+//   // Future Signup () async {
+//   //   try{
+//   //     await FirebaseAuth.instance.createUserWithEmailAndPassword(email: "dody@gmail.com", password: "123");
+//   //
+//   //   }
+//   //  on FirebaseException catch(e) {
+//   //   // ignore: avoid_print
+//   //   print("haidy");
+//   //
+//   //   }
+//   // }
+//
+//
+class Authentication {
+
+  static Future<FirebaseApp> initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  static void signup(BuildContext context,String email, String password) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    auth.createUserWithEmailAndPassword(
+        email: email, password: password).then((_) {
+      print("registeration success");
+      //   Navigator.of(context).pushReplacement(
+      //       // MaterialPageRoute(builder: (context) => VerifyScreen()));
+      // });
+    });
+  }
+
+  static void signin(BuildContext context, String email, String password) {
+    final auth = FirebaseAuth.instance;
+    auth.signInWithEmailAndPassword(email: email, password: password).then((_) {
+      // Navigator.of(context).pushReplacement(
+      //     MaterialPageRoute(builder: (context) => HomePage())
+      // );
+      print("Success logged in");
+    });
+  }
+
+}
+//
+// class Signin extends StatelessWidget {
+//   const Signin({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: InkWell
+//         (
+//           onTap: (){
+//             Authentication.signup(context, "dd@gmail.co", "password");
+//           },
+//           child: Container(
+//             width: 100,
+//             height: 100,
+//             color: Colors.red,
+//
+//           )),
+//     );
+//   }
+// }
